85d88ac47012d49a2173f355c298812dd0c93ba0
Ilya Danilov <peertosir@gmail.com>
2019-11-11 01:10:18 +0300

    Models added

---
+++ learn_peer_app/models.py
@@ -0,0 +1,102 @@
+from learn_peer_app import db
+from werkzeug.security import generate_password_hash
+from datetime import datetime, date
+
+
+course_students_maps = db.Table(
+    'course_students_maps',
+    db.Column('student_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('course_id', db.Integer, db.ForeignKey('course.id'))
+)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    status = db.Column(db.String(20), nullable=False)
+    phone = db.Column(db.String(20))
+    skype = db.Column(db.String(40))
+    reg_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+    courses_mentor = db.relationship('Course', backref='course_mentor', lazy='dynamic')
+    courses_part = db.relationship('Course', secondary=course_students_maps,
+                                   backref='course_students', lazy='dynamic')
+    tasks_author = db.relationship('Task', backref='task_author', lazy='dynamic')
+    tasks_executor = db.relationship('Task', backref='task_exec', lazy='dynamic')
+    lecture_author = db.relationship('Lecture', backref='lecture_author', lazy='dynamic')
+
+    def __init__(self, username, email, password, status, phone='', skype=""):
+        self.username = username
+        self.email = email
+        self.password = generate_password_hash(password)
+        self.status = status
+        self.phone = phone
+        self.skype = skype
+
+    def __repr__(self):
+        return f"User: {self.username}, status: {self.status}"
+
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), nullable=False)
+    description = db.Column(db.String(300), nullable=False)
+    date_start = db.Column(db.DateTime, default=datetime.utcnow(), nullable=False)
+    date_finish = db.Column(db.DateTime, nullable=False)
+    mentor_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    max_participants = db.Column(db.Integer, nullable=False)
+    lectures = db.relationship('Lecture', backref='course')
+
+    def __init__(self, title, description, mentor_id, max_participants):
+        self.title = title
+        self.description = description
+        self.max_participants = max_participants
+        self.mentor_id = mentor_id
+
+    def __repr__(self):
+        return f"Course {self.title} by {self.course_mentor.username}"
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    status = db.Column(db.String(20), nullable=False, default='Opened')
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    date_to_do = db.Column(db.DateTime)
+    answer = db.Column(db.Text)
+
+    def __init__(self, title, description, author_id, executor_id):
+        self.title = title
+        self.description = description
+        self.author_id = author_id
+        self.executor_id = executor_id
+
+    def __repr__(self):
+        return f"Task from {self.task_author.username} for {self.task_exec.username}"
+
+
+
+class Lecture(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+    tasks = db.relationship('Task', backref='to_lecture')
+    date_to_happen = db.Column(db.DateTime)
+
+    def __init__(self, title, content, author_id, course_id):
+        self.title = title
+        self.content = content
+        self.author_id = author_id
+        self.course_id = course_id
+
+    def __repr__(self):
+        return f"Lecture {self.title} for course {self.course.title}"
+
+
+
+
