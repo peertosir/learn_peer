7f3b6f64518b8dfb4a1c5f2007af951f454e32db
Ilya Danilov <peertosir@gmail.com>
2019-11-16 21:53:01 +0300

    added error pages handling

--- learn_peer_app/__init__.py
+++ learn_peer_app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -29,3 +29,18 @@ app.register_blueprint(core)
 app.register_blueprint(courses)
 app.register_blueprint(tasks)
 app.register_blueprint(lectures)
+
+
+#Error handlers
+@app.errorhandler(403)
+def not_allowed_error(error):
+    return render_template('errors/404.html'), 403
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('errors/html'), 500
